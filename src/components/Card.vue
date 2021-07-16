37b62491d269e4fc339cc77d09042fac69cc8881
Gurdeep Singh <gurdeepsingh2000329@gmail.com>
2021-07-16 17:16:44 +0200

    update

---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="cover">
+        <img src="https://image.tmdb.org/t/p/w342/hQq8xZe5uLjFzSBt4LanNP7SQjl.jpg">
+        <div class="inner">
+                <h2>{{title}}</h2>
+                <h3>Titolo Originale: {{original_title}}</h3>
+                <h4>Lingua: {{original_language}}</h4>
+                <h3>Voto: {{vote_average}}</h3>
+            </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Card',
+        props:{
+        original_title:String,
+        title:String,
+        original_language:String,
+        vote_average:Number,
+    },
+    }
+</script>
+
+<style lang="scss" scoped>
+.inner{
+    h2,h2,h3,h4{
+        color: white;
+    }
+}
+</style>
